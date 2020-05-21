1483952435b044d7a5a39cd5ef9ede5a14ece5a8
Ravuthz <ravuthz@gmail.com>
2020-05-22 00:14:18 +0700

    Apply basic crud on user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,21 @@
 import { Controller } from '@nestjs/common';
+import { UserEntity } from './user.entity';
+import { CrudController, Crud } from '@nestjsx/crud';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Crud({
+	model: {
+		type: UserEntity
+	},
+	query: {
+		cache: 2000,
+		limit: 1,
+		maxLimit: 100,
+		exclude: ['password'],
+		alwaysPaginate: true
+	}
+})
+@Controller('users')
+export class UserController implements CrudController<UserEntity> {
+    constructor (public service: UserService) {}
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,11 @@
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { Injectable } from '@nestjs/common';
+import { UserEntity } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService extends TypeOrmCrudService<UserEntity> {
+    constructor(@InjectRepository(UserEntity) repo) {
+        super(repo);
+    }
+}
