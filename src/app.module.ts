302d3a4236c2b2fa7c1948280111ce0dbed509fa
Ravuthz <ravuthz@gmail.com>
2020-05-21 21:56:49 +0700

    Add user module by nest-js cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 import app from './config/app';
 import database from './config/database';
@@ -17,7 +18,8 @@ import database from './config/database';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (config: ConfigService) => config.get('database')
-    })
+    }),
+    UserModule
   ],
   exports: [ConfigModule],
   controllers: [AppController],
